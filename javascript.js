11886fde7bbf33a28a9b5d1f643f87a8957d641f
Luan Furlan <luan.luan2009@hotmail.com>
2024-08-21 22:06:18 -0300

    v01
    Added conversions. Button changes. Changes in values.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,9 @@
+//Elements
+
 const convertFor = document.querySelector('#convert-for');
 convertFor.addEventListener('click', changingCurrency);
+const convertMoney = document.querySelector('button');
+
 
 //Images For 
 const imgForDolar = document.querySelector('img[alt*="dolar"]');
@@ -14,20 +18,67 @@ function removeActiveClass() {
     imgForLibra.classList.remove('active');
 }
 
+// switch image and currency name
 function changingCurrency() {
-    console.log('clique')
-    const convertForValue = document.querySelector('#convert-for').value;
+    const valueFromHTML = document.querySelector('.currencyFromName');
+    const valueFromNum = document.querySelector('.currencyFromValue');
     if(convertFor.value === 'dolar') {
         removeActiveClass();
         imgForDolar.classList.add('active');
+        valueFromHTML.innerHTML = 'Dolar Americano'
+        valueFromNum.innerHTML = `U$ 0,00`;
     } else if(convertFor.value === 'euro') {
         removeActiveClass();
         imgForEuro.classList.add('active');
+        valueFromHTML.innerHTML = 'Euro'
+        valueFromNum.innerHTML = `€ 0,00`;
     } else if(convertFor.value === 'bitcoin') {
         removeActiveClass();
         imgForBit.classList.add('active');
+        valueFromHTML.innerHTML = 'Bitcoin';
+        valueFromNum.innerHTML = `BTC 0,00`;
     } else if(convertFor.value === 'libra') {
         removeActiveClass();
         imgForLibra.classList.add('active');
+        valueFromHTML.innerHTML = 'Libra';
+        valueFromNum.innerHTML = `£ 0,00`;
     }
 }
+
+convertMoney.addEventListener('click', conversionMoney)
+
+function conversionMoney() {
+    const convertFromValue = +document.querySelector('#convertValue').value.replace(',', '.');
+    const convertedFromValue = document.querySelector('.currencyForValue');
+    const cotDolar = 5.42;
+    const cotEuro = 6.11;
+    const cotLibra = 7.18;
+    const cotBit = 334314.93;
+
+    //mostrar o BRL.
+    const formattedValue = convertFromValue.toLocaleString('pt-BR', {
+        style: 'currency',
+        currency: 'BRL',
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2
+    });
+    const currencyValueFrom = document.querySelector('.currencyFromValue');
+    currencyValueFrom.innerHTML = formattedValue
+    console.log(formattedValue)
+    
+    // conversor
+    if(convertFor.value === "dolar") {
+        const convertedValue = (convertFromValue / cotDolar).toFixed(2);
+        convertedFromValue.innerHTML = `U$ ${convertedValue}`;
+    } else if(convertFor.value === "euro") {
+        const convertedValue = (convertFromValue / cotEuro).toFixed(2);
+        convertedFromValue.innerHTML = `€ ${convertedValue}`;
+    } else if(convertFor.value === "libra") {
+        const convertedValue = (convertFromValue / cotLibra).toFixed(2);
+        convertedFromValue.innerHTML = `£ ${convertedValue}`;
+    } else if(convertFor.value === "bitcoin") {
+        const convertedValue = (convertFromValue / cotBit).toFixed(2);
+        convertedFromValue.innerHTML = `BTC ${convertedValue}`;
+    }
+};
+
